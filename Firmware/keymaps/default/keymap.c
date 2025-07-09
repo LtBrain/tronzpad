35929d6816c2365a413a04050b0819bb42ff65d6
LtBrain <brian.j.guan@outlook.com>
2025-07-09 13:27:01 -0400

    encoder and VID/PID addition to firmware

--- Firmware/keymaps/default/keymap.c
+++ Firmware/keymaps/default/keymap.c
@@ -5,19 +5,30 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
+     * ┌───┬───┬───┬───┐  ┌───┐
+     * │ 1 │ 2 │ 3 │ 4 │ ││ E ││
+     * ├───┼───┼───┼───┤  └───┘ 
+     * │ 5 │ 6 │ 7 │ 8 │
      * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * │ 9 │ 0 │SPC│ENT│
+     * └───┴───┴───┴───┘ 
+    
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS
+        KC_1,   KC_2,   KC_3,   KC_4, DF(1),
+        KC_5,   KC_6,   KC_7,   KC_8,
+        KC_9,   KC_0,   KC_SPC, KC_ENTER
+    ),
+    [1] = LAYOUT(
+        KC_Q,   KC_W,   KC_E,   KC_R, DF(0),
+        KC_A,   KC_S,   KC_D,   KC_F,
+        KC_Z,   KC_X,   KC_C,   KC_F
     )
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) }
+};
+#endif
